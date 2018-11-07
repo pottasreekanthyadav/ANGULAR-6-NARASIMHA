4b079ad61245e11514068c067778073da7c249f1
srikant <pottasreekanthyadav@gmail.com>
2018-11-07 21:16:10 +0530

    Testing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
 import { NgModule } from '@angular/core';
 
+import { Rangepipe } from './custompipe/rangepipe';
 import { AppComponent } from './app.component';
+import { DemoComponent } from './demo.component';
+import { EmpComponent } from './emp/emp.component';
+import { StudentComponent } from './student/student.component';
+import { RamComponent } from './ram/ram.component';
+import { PipesComponent } from './pipes/pipes.component';
+import { BindingComponent } from './binding/binding.component';
+import { SaiComponent } from './sai/sai.component';
+import { CustompipeComponent } from './custompipe/custompipe.component';
+import { TemplateformComponent } from './templateform/templateform.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,DemoComponent,Rangepipe, EmpComponent, StudentComponent, RamComponent, PipesComponent, BindingComponent, SaiComponent, CustompipeComponent, TemplateformComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/templateform/templateform.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'app-templateform',
+  templateUrl: './templateform.component.html',
+  styleUrls: ['./templateform.component.css']
+})
+export class TemplateformComponent  {
+public fname:string="";
+public lname:string="";
+  
+}
