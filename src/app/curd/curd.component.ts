ac468227e87e7287f2c9fba5d426123d1fbb3cae
srikant <pottasreekanthyadav@gmail.com>
2019-05-16 13:18:58 +0530

    first commit

--- src/app/curd/curd.component.ts
+++ src/app/curd/curd.component.ts
@@ -2,8 +2,8 @@ import { Component } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 @Component({
   selector: 'app-curd',
-  templateUrl: './curd.component.html',
-  styleUrls: ['./curd.component.css']
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
 export class CurdComponent {
 
